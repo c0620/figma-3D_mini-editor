a9f886b2d6f479c1675fe2a81b93e94ffdab1663
c0620 <e.shimanskua@gmail.com>
2026-05-11 04:26:21 +0300

    feat: add simple MemoryRouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,16 @@
-import { useState } from "react";
-import { postMessageToBackend } from "./lib/helpers";
+import { Routes, Route, MemoryRouter } from "react-router";
+import StartScreen from "./ui/components/screens/StartScreen";
+import EditorPage from "./ui/components/screens/EditorPageScreen";
 
 function App() {
-  const [count, setCount] = useState(5);
-  const createRect = () => {
-    postMessageToBackend("create-rectangles", { count: count });
-  };
-
-  const onCancel = () => {
-    postMessageToBackend("cancel");
-  };
-
   return (
     <>
-      <div className="panel">
-        <h1 className="panel-title">🚀 Figma Plugin Starter</h1>
-        <p className="panel-desc">
-          Use this panel to interact with your plugin. Update controls as needed.
-        </p>
-
-        <input
-          id="count"
-          type="number"
-          value={count}
-          onChange={(e) => setCount(Number(e.target.value))}
-          className="input"
-        />
-
-        <button onClick={createRect} className="button button-blue">
-          Create Rectangle
-        </button>
-
-        <button onClick={onCancel} className="button button-red">
-          Cancel
-        </button>
-      </div>
+      <MemoryRouter>
+        <Routes>
+          <Route index path="/" element={<StartScreen />} />
+          <Route path="/editor" element={<EditorPage />} />
+        </Routes>
+      </MemoryRouter>
     </>
   );
 }
