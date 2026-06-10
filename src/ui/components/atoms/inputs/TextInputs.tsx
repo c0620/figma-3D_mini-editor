af73ee1839b77acd600caa6b8330e6f3201e2578
cmg <e.shimanskua@gmail.com>
2026-06-10 18:22:10 +0300

    feat: restore previous InputText value when left empty

---
+++ src/ui/components/atoms/inputs/TextInputs.tsx
@@ -0,0 +1,57 @@
+import type { ChangeEvent } from "react";
+
+import styles from "./TextInputs.module.scss";
+import { useContext, useState } from "react";
+
+import clsx from "clsx";
+
+export type InputField = {
+  onChange: (value: number) => void;
+  value: number;
+  isActive: boolean;
+  label?: string;
+  range?: {
+    min: number;
+    max: number;
+    step: number;
+    variant: "default" | "centered";
+  };
+};
+
+export function InputText({ field }: { field: InputField }) {
+  const [cachedValue, setCachedValue] = useState(field.value);
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const parsed = Number.parseFloat(e.target.value.replace(",", "."));
+    setCachedValue(parsed);
+  };
+
+  const setChange = () => {
+    if (Number.isFinite(cachedValue)) field.onChange(cachedValue);
+    else setCachedValue(field.value);
+  };
+
+  return (
+    <div className={styles.inputText}>
+      {field.label ? (
+        <label className={styles.inputTextLabel}>{field.label}</label>
+      ) : null}
+      <input
+        className={styles.inputTextField}
+        type="number"
+        style={field.isActive ? { color: "orange" } : { color: "white" }}
+        onInput={handleChange}
+        onKeyDown={(e) => {
+          if (e.code === "Enter") setChange();
+        }}
+        value={cachedValue}
+        onBlur={setChange}
+      />
+    </div>
+  );
+}
+
+export function InputMultipleText() {}
+
+export function InputProjectName() {}
+
+export function InputModelSource() {}
